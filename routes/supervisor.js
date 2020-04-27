ee628c0a4121954b3d48c08ee9e39ced5ea4212d
Glitch (unite-dev) <none>
2020-04-27 09:46:39 +0000

    🌞🙎 Updated with Glitch

--- routes/supervisor.js
+++ routes/supervisor.js
@@ -14,6 +14,7 @@ router.get("/", isAuth, sessionExit, supervisorController.getIndex);
 router.get("/sign-in", supervisorController.getSignIn);
 router.get("/sign-up", supervisorController.getSignUp);
 router.get("/profile", isAuth, sessionExit, supervisorController.getProfile);
+
 router.post("/sign-in", supervisorController.postSignIn);
 router.post("/sign-up", supervisorController.postSignUp);
 module.exports = router;
